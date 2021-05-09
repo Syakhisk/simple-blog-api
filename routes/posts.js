bfe5993bb17559962b6eae854b16caf3067f3831
syakhisk <mail.syakhisk@gmail.com>
2021-05-10 02:33:02 +0700

    Update Endpoint Posts & Users

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,78 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const prisma = require("../lib/prisma");
+const { body, check, validationResult } = require("express-validator");
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-	res.send("respond with a resource");
+router.get("/", async (req, res, next) => {
+	let data;
+	const page = req.query.page;
+	if (page) {
+		data = await posts({ page });
+	}
+	data = await posts();
+	if (data) {
+		res.send(data);
+	}
 });
+
+router.get("/:id", async (req, res, next) => {
+	const id = req.params.id;
+	const data = await posts(id);
+	if (data) {
+		res.send(data);
+	} else {
+		res.status(404);
+		res.send({ message: "Post not found!" });
+	}
+});
+
+const newPostValidation = [
+	// check("username").notEmpty().bail().isLength({ min: 6, max: 12 }).bail(),
+	// check("email").isEmail().bail(),
+	// check("password").notEmpty().bail().isLength({ min: 8, max: 24 }).bail(),
+];
+router.post("/", newPostValidation, async (req, res, next) => {
+	const id = req.params.id;
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		res.status(422).json({ errors: errors.array() });
+		return;
+	}
+
+	if (req.body) {
+		res.send(req.body);
+	} else {
+		res.send({ msg: "lao" });
+	}
+});
+
+const posts = async (args) => {
+	let data;
+
+	if (!args) {
+		data = await prisma.posts.findMany({
+			take: 10,
+		});
+
+		return data;
+	}
+
+	const { id, page = 1 } = args;
+
+	if (id) {
+		data = await prisma.posts.findUnique({
+			where: {
+				id: Number(id),
+			},
+		});
+	} else if (page) {
+		data = await prisma.posts.findMany({
+			take: page * 10,
+		});
+	}
+
+	return data;
+};
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,90 @@
 const express = require("express");
-const get_user = require("../lib/get_users");
+const prisma = require("../lib/prisma");
+const { body, check, validationResult } = require("express-validator");
 const router = express.Router();
 
-/* GET users listing. */
 router.get("/", async (req, res, next) => {
-	const users = await get_user();
-	res.send({
-		code: 200,
-		data: users,
-	});
+	const data = await users();
+	res.send(data);
 });
 
 router.get("/:id", async (req, res, next) => {
 	const id = req.params.id;
-	const user = await get_user(id);
+	const data = await users(id);
 
-	// console.log(user);
-	if (user) {
-		res.send({
-			code: 200,
-			data: user,
-		});
+	if (data) {
+		res.send(data);
 	} else {
+		res.status(404);
 		res.send({
-			code: 404,
-			data: "User not found!",
+			msg: "User not found!",
 		});
 	}
 });
+
+const newUserValidation = [
+	check("username")
+		.notEmpty()
+		.withMessage("is required!")
+		.isLength({ min: 5, max: 24 }),
+	check("password")
+		.notEmpty()
+		.withMessage("is required!")
+		.isLength({ min: 8, max: 36 }),
+];
+
+router.post("/", newUserValidation, async (req, res, next) => {
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		res
+			.status(422)
+			.json({ errors: errors.array().map((e) => `${e.param} ${e.msg}`) });
+		return;
+	}
+
+	const db = await newUser(req.body);
+
+	res.send(req.body);
+});
+
+const newUser = async (body) => {
+	let data = await prisma.users.create({
+		data: {
+			username: body.username,
+			password: body.password,
+			email: body.email,
+		},
+	});
+
+	console.log(data);
+
+	return data;
+};
+
+const users = async (id = null) => {
+	let data;
+	const select = {
+		id: true,
+		username: true,
+		name: true,
+		created_at: true,
+		updated_at: true,
+	};
+
+	if (id) {
+		data = await prisma.users.findUnique({
+			where: {
+				id: Number(id),
+			},
+			select,
+		});
+	} else {
+		data = await prisma.users.findMany({
+			select,
+		});
+	}
+
+	return data;
+};
 module.exports = router;
