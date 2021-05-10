fedbeba88b955b571cc6a3502d5a66d2e10b1ac8
syakhisk <mail.syakhisk@gmail.com>
2021-05-10 18:35:35 +0700

    Menambahkan Error Handling u/ Invalid JSON Request

--- app.js
+++ app.js
@@ -16,6 +16,16 @@ const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
+
+app.use(function (err, req, res, next) {
+	console.error(err.stack);
+	if (err.type == "entity.parse.failed") {
+		res.status(500).send({ msg: "Invalid JSON request!" });
+		return;
+	}
+	res.status(500).send({ msg: "Something went wrong!" });
+});
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
